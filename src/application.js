1824970574e8a3111a7875dc14c50eca747048b9
Christopher Duke <cd.duke@gmail.com>
2016-07-15 15:22:33 -0500

    help from ryan

--- src/application.js
+++ src/application.js
@@ -1,11 +1,24 @@
 export default class ApplicationView {
-  constructor (htmlApp) {
-    this.htmlApp = htmlApp;
+  constructor(element) {
+    this.element = element;
     this.data = [];
   }
   render() {
-    this.htmlApp.innerHTML = '';
+    const list = this.element.querySelector('.puppy-list');
+    list.innerHTML = '';
+    debugger;
 
-
+    this.data.forEach((puppy) => {
+      const newPuppy = new PuppyView(puppy, this);
+      list.appendChild(newPuppy.element);
+    });
+  }
+  start() {
+    return fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.data = data;
+        this.render();
+      })
   }
 }
--- src/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export default function index() {
-  console.log('dan is an ass sometimes but hes not all bad')
-}
